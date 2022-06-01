303e8a8feb031a469d5bfdd80eb91df97bd3a640
Evandromck <evandroaraujomck@gmail.com>
2022-06-01 15:53:06 -0300

    Deploy secao 2 aula 8

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import Finalnome from './componet/finalnome';
-import Casa from './componet/nome';
-import Sobrenome from './componet/sobrenome';
-import Nomeca from './componet/nomeca';
-import Nomepro from './componet/nomeprops';
-import Nomepr from './componet/Nomepr';
-import Nomefinal from './componet/nomefinal'
+import { useState } from 'react';
+
+
 
 function App(){
+  
+   
+ 
+
+  
+
   return(
     <div>
-      <h1>Componente App</h1>
-      <Casa />
-      <Sobrenome nome="Araujo"/>
-      <Finalnome nome="Pereira"/>
-      <Nomeca />
-      <Nomepro text="=>  Esse texto esta dentro de uma props "/>
-      <Nomepr />
-      <Nomefinal texnemo=" => texto com a troca do props" />
+      <h1>Cadastrabdo usuario</h1>
+
+      <form>
+        <label>Nome:</label><br/>
+        <input placeholder="Digite seu nome" /><br/>
+
+        <label>email:</label><br/>
+        <input placeholder="Digite seu meil" /><br/>
+
+        <label>idade:</label><br/>
+        <input placeholder="Digite seu idade" /><br/>
+
+        <button type="submit">registrar</button>
+
+      </form>
+
+      
+
    </div>
   );    
 }
